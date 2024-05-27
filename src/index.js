80842ea5efc9a23d70697a28f68f1e6cf4c77dc5
lnbEli <137424458+lnbEli@users.noreply.github.com>
2024-05-27 13:20:52 +0200

    Add photos. Add JS that measures image size so that when image is resized the frame will adjust dynamically. Creat a wide div where all the pictures will be placed in a row

--- src/index.js
+++ src/index.js
@@ -1,2 +1,33 @@
 import "./style.css";
-console.log("test");
+
+function adjustFrameSizeAndPosition() {
+  const frameDiv = document.querySelector(".frame");
+  const wideDiv = document.querySelector(".wide");
+  const firstImage = document.querySelector(".imgOne");
+
+  const firstImageRect = firstImage.getBoundingClientRect();
+  const imageWidth = firstImageRect.width;
+  const imageHeight = firstImageRect.height;
+
+  // Adjust the frame size to match the image size
+  frameDiv.style.width = `${imageWidth}px`;
+  frameDiv.style.height = `${imageHeight}px`;
+
+  // Adjust this index to choose a different image
+  changeImage(1);
+}
+
+// Initial adjustment
+adjustFrameSizeAndPosition();
+// Adjust on window resize
+window.addEventListener("resize", adjustFrameSizeAndPosition);
+
+function changeImage(index) {
+  const firstImage = document.querySelector(".imgOne");
+  const firstImageRect = firstImage.getBoundingClientRect();
+  const imageWidth = firstImageRect.width;
+  const wideDiv = document.querySelector(".wide");
+  const offset = -index * imageWidth;
+
+  wideDiv.style.transform = `translateX(${offset}px)`;
+}
